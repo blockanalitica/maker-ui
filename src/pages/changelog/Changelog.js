7c4fae38464d7ec334a9d28c96c526d8309d43bf
Tomaz Sifrer <tomazz.sifrer@gmail.com>
2022-10-20 07:44:48 +0200

    Updated changelog

--- src/pages/changelog/Changelog.js
+++ src/pages/changelog/Changelog.js
@@ -19,10 +19,19 @@ function Changelog(props) {
     <>
       <h1 className="h3 mb-4">changelog</h1>
       <div className={styles.section}>
-        <h3 className={styles.date}>2022-10-13</h3>
+        <h3 className={styles.date}>October 2022</h3>
         <h4 className={styles.changeSection}>Added</h4>
         <ul className={styles.changes}>
+          <li>Added wallet view for showing all vaults from the same owner</li>
+          <li>Linking owner addresses to their ENS</li>
           <li>New changelog page for tracking notable changes to this project</li>
+        </ul>
+        <h4 className={styles.changeSection}>Changed</h4>
+        <ul className={styles.changes}>
+          <li>
+            Imporoved the logic for linking a vault to it's owner when they are using ds
+            proxy
+          </li>
         </ul>
       </div>
     </>
